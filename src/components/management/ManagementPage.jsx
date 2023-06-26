f52f7f1d231f163b6e3fa5b2e76c4059963db547
nurcanNT <nrcntpkra@gmail.com>
2023-06-26 20:01:32 +0300

    management css update

--- src/components/management/ManagementPage.jsx
+++ src/components/management/ManagementPage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap stil dosyasını ekleyin
 import './management.css'; // Özel stil dosyasınızı ekleyin (örneğin, styles.css)
+import React, { useState } from 'react';
+import FormList from './FormList';
 
-const DynamicForm = () => {
+const ManagementPage = () => {
   const [showPopup, setShowPopup] = useState(false);
 
   const handleOpenPopup = () => {
@@ -14,9 +16,10 @@ const DynamicForm = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <button className="btn btn-primary" onClick={handleOpenPopup}>Yeni Form Oluştur</button>
+    <div>
+      <button onClick={handleOpenPopup}>Yeni Form Oluştur</button>
       {showPopup && <FormPopup onClosePopup={handleClosePopup} />}
+      <FormList />
     </div>
   );
 };
@@ -99,71 +102,71 @@ const FormPopup = ({ onClosePopup }) => {
 
   return (
     <div className="popup">
-      <div className="popup-inner">
-        <h2>Yeni Form Oluştur</h2>
-        <div className="form-group">
-          <label htmlFor="name">Form Adı:</label>
-          <input
-            id="name"
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            className="form-control"
-          />
-          {formErrors.name && <span className="error">{formErrors.name}</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="description">Açıklama:</label>
-          <input
-            id="description"
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-            className="form-control"
-          />
-        </div>
-        <h3>Form Alanları</h3>
-        {formFields.map((field, index) => (
-          <div key={index} className="field">
-            <div className="form-group">
-              <label htmlFor={`fieldName-${index}`}>Alan Adı:</label>
-              <input
-                id={`fieldName-${index}`}
-                type="text"
-                name="name"
-                value={field.name}
-                onChange={(e) => handleFieldChange(index, e)}
-                className="form-control"
-              />
-              {formErrors[`field-${index}-name`] && (
-                <span className="error">{formErrors[`field-${index}-name`]}</span>
-              )}
-            </div>
-            <div className="form-group">
-              <label htmlFor={`fieldDataType-${index}`}>Veri Tipi:</label>
-              <input
-                id={`fieldDataType-${index}`}
-                type="text"
-                name="dataType"
-                value={field.dataType}
-                onChange={(e) => handleFieldChange(index, e)}
-                className="form-control"
-              />
-              {formErrors[`field-${index}-dataType`] && (
-                <span className="error">{formErrors[`field-${index}-dataType`]}</span>
-              )}
-            </div>
-            <button className="btn btn-danger" onClick={() => handleRemoveField(index)}>Alanı Kaldır</button>
-          </div>
-        ))}
-        <button className="btn btn-success" onClick={handleAddField}>Alan Ekle</button>
-        <button className="btn btn-primary" onClick={handleSaveForm}>Kaydet</button>
-        <button className="btn btn-secondary" onClick={onClosePopup}>İptal</button>
+    <div className="popup-inner">
+      <h2>Yeni Form Oluştur</h2>
+      <div className="form-group">
+        <label htmlFor="name">Form Adı:</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          className="form-control"
+        />
+        {formErrors.name && <span className="error">{formErrors.name}</span>}
       </div>
+      <div className="form-group">
+        <label htmlFor="description">Açıklama:</label>
+        <input
+          id="description"
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          className="form-control"
+        />
+      </div>
+      <h3>Form Alanları</h3>
+      {formFields.map((field, index) => (
+        <div key={index} className="field">
+          <div className="form-group">
+            <label htmlFor={`fieldName-${index}`}>Alan Adı:</label>
+            <input
+              id={`fieldName-${index}`}
+              type="text"
+              name="name"
+              value={field.name}
+              onChange={(e) => handleFieldChange(index, e)}
+              className="form-control"
+            />
+            {formErrors[`field-${index}-name`] && (
+              <span className="error">{formErrors[`field-${index}-name`]}</span>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor={`fieldDataType-${index}`}>Veri Tipi:</label>
+            <input
+              id={`fieldDataType-${index}`}
+              type="text"
+              name="dataType"
+              value={field.dataType}
+              onChange={(e) => handleFieldChange(index, e)}
+              className="form-control"
+            />
+            {formErrors[`field-${index}-dataType`] && (
+              <span className="error">{formErrors[`field-${index}-dataType`]}</span>
+            )}
+          </div>
+          <button className="btn btn-danger" onClick={() => handleRemoveField(index)}>Alanı Kaldır</button>
+        </div>
+      ))}
+      <button className="btn btn-success" onClick={handleAddField}>Alan Ekle</button>
+      <button className="btn btn-primary" onClick={handleSaveForm}>Kaydet</button>
+      <button className="btn btn-secondary" onClick={onClosePopup}>İptal</button>
     </div>
+  </div>
   );
 };
 
-export default DynamicForm;
+export default ManagementPage;
