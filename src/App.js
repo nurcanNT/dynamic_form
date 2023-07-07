81309386ddae0299e5a6b99f4b5ece9b6bf4c922
nurcanNT <nrcntpkra@gmail.com>
2023-07-07 17:07:39 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import React, { Suspense } from "react";
-import FormsPage from './features/forms/FormsPage';
 
 const Forms = React.lazy(() => import("./pages/forms"));
 const FormDetail = React.lazy(() => import("./pages/formDetail"));
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error:", error);
+    console.error("Error Info:", errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops! Bir hata oluştu.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<FormsPage />} />
-          <Route path="/" element={<Forms />} />
-          <Route path="/forms/:name" element={<FormDetail />} />
-          <Route path="/forms/:name" element={<FormsPage/>} />
-        </Routes>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" element={<Forms />} />
+            <Route path="/forms/:name" element={<FormDetail />} />
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
     </Router>
   );
 }
